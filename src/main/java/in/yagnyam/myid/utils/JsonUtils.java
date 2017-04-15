286199809a068dbe5e6a16ff020a83703c3e1204
Chenna Reddy <chenna@alavala.in>
2017-04-15 13:04:50 +0200

    added javadoc

--- src/main/java/in/yagnyam/myid/utils/JsonUtils.java
+++ src/main/java/in/yagnyam/myid/utils/JsonUtils.java
@@ -22,16 +22,35 @@ public class JsonUtils {
         return mapper;
     }
 
+    /**
+     * Converts Java Object o JSON String
+     * @param o Any Java Object
+     * @return Object as JSON String
+     */
     @SneakyThrows(IOException.class)
     public static String toJson(Object o) {
         return getObjectMapper().writeValueAsString(o);
     }
 
+    /**
+     * Converts JSON String to Java Object
+     * @param jsonString JSON String
+     * @param destinationClass Class that must be created by reading JSON String
+     * @param <T> Class type for creating result Object
+     * @return Java object populated from JSON String
+     */
     @SneakyThrows(IOException.class)
     public static <T> T fromJson(String jsonString, Class<T> destinationClass) {
         return getObjectMapper().readValue(jsonString, destinationClass);
     }
 
+    /**
+     * Converts one Java Object to another Java Object of different type with same fields using JSON
+     * @param from The Object that should be converted to
+     * @param destinationClass Class that must be created from original Object
+     * @param <T>
+     * @return Copy of from Object as an Object of destination class
+     */
     @SneakyThrows(IOException.class)
     public static <T> T convert(Object from, Class<T> destinationClass) {
         return getObjectMapper().readValue(toJson(from), destinationClass);
--- src/main/java/in/yagnyam/myid/utils/StringUtils.java
+++ src/main/java/in/yagnyam/myid/utils/StringUtils.java
@@ -2,14 +2,25 @@ package in.yagnyam.myid.utils;
 
 
 /**
- * Json Utility methods
+ * String Utility methods
  */
 public class StringUtils {
 
+    /**
+     * Safely check if a string is empty or null
+     *
+     * @param s String input to check
+     * @return true if string is null or empty
+     */
     public static boolean isEmpty(String s) {
         return s == null || s.trim().isEmpty();
     }
 
+    /**
+     * Return first non null string from the arguments
+     * @param strings Strings to find non-null
+     * @return First non null string or empty string all arguments are null
+     */
     public static String nonNull(String... strings) {
         if (strings == null) {
             return "";
@@ -22,6 +33,12 @@ public class StringUtils {
         return "";
     }
 
+    /**
+     * Safely check if two string are equal or not
+     * @param a One of the strings in comparison
+     * @param b Other string in comparison
+     * @return true if both are pointing to same address (possibly null) or equal in content. False otherwise.
+     */
     public static boolean equals(String a, String b) {
         return a == b || (a != null && a.equals(b));
     }
